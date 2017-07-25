0b78dd1e4dedf7d2f1c4e92091f0c1a2b097dbd1
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-25 11:47:18 -0400

    refining the perceptron

--- code/experimental/NAND_gate_perceptron_new.py
+++ code/experimental/NAND_gate_perceptron_new.py
@@ -3,6 +3,10 @@ import numpy as np
 def activation(val):
 	return 1 if val>0.0 else 0
 
+def train(self, X, Y, W):
+	for n in xrange(10):
+		for d in xrange(X.shape[0]):
+			
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 Y = np.array([[1],[1],[1],[0]])
 biases = np.ones((4,1))
@@ -10,6 +14,6 @@ X = np.hstack((X,biases))
 
 weights = np.random.randn(3)
 
-print activation(0), activation(-2.0), activation(1.2),
+print activation(np.dot(X[0],weights))
 
 
