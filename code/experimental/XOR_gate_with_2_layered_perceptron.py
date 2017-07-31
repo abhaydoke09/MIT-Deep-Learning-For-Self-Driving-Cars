3e5f51b8f714e78385ce054aa236c7d64c725b61
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-31 18:10:50 -0400

    onr more step

--- code/experimental/XOR_gate_with_2_layered_perceptron.py
+++ code/experimental/XOR_gate_with_2_layered_perceptron.py
@@ -3,6 +3,7 @@ import math
 
 sigmoid = lambda x: 1/(1 + np.exp(-x))
 relu = lambda x: x*(x>0)
+threshold = lambda x: 1*(x>0)
 
 def activation(val):
 	return 1 if val>0.0 else 0
@@ -21,9 +22,50 @@ def predict(X, Y, W):
 		print '{:^12}'.format(X[d]), '{:^9}'.format(activation(np.dot(X[d],W))), '{:^6}'.format(Y[d])
 
 def loss(Y, target):
-	return (Y - target)**2
+	return 0.5*(Y - target)**2
 
 
+def predict():
+	for n in range(X.shape[0]):
+		#print weights_1.shape, X[0][:,np.newaxis].shape, b1.shape
+		a1 = relu((np.dot(weights_1, X[n][:,np.newaxis]))+b1)
+		#print a1.shape
+		#print weights_2.shape, a1.shape, b2.shape
+		a2 = threshold((np.dot(weights_2, a1))+b2)
+
+		print X[n], Y[n], a2
+
+
+
+def backprop(X, Y, weights_1, b1, a1, weights_2, b2, a2, l):
+	#print a2.shape
+	da2 = a2*(a2>0)*(l*0.5**0.5)
+	print 'da2 = ',da2,'\n'
+	#print da2.shape
+	
+	dweights_2 = learning_rate*da2[0]*a1
+	#print weights_2.shape, dweights_2.T.shape
+	#print weights_2
+	weights_2 -= dweights_2.T
+	#print weights_2
+
+	db2 = learning_rate*da2
+	b2 -= db2
+
+	
+	#print dweights_2.shape, a1.shape
+	da1 = dweights_2*(a1>0)
+	#print da1.shape
+	#print weights_1*da1*X
+
+	#print weights_1.shape, da1.shape, X.shape
+	dweights_1 = learning_rate*(weights_1*da1)*X[:np.newaxis]
+	#print dweights_1.shape
+	weights_1 -= dweights_1
+	db1 = learning_rate*da1
+	b1 -= db1
+
+	return (weights_1, b1, weights_2, b2)
 
 
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
@@ -31,11 +73,27 @@ Y = np.array([[0],[1],[1],[0]])
 
 
 weights_1 = np.random.randn(3,2)
-b1 = np.random.randn(3)
+b1 = np.random.randn(3,1)
 weights_2 = np.random.randn(1,3)
-b2 = np.random.randn(1)
+b2 = np.random.randn(1,1)
+learning_rate = 1.0
 
-for n in range(X.shape[0]):
-	a1 = relu((np.dot(weights_1, X[n]))+b1)
-	a2 = relu((np.dot(weights_2, a1))+b2)
-	print loss(a2, Y[n])
+for i in range(1):
+	for n in range(X.shape[0]):
+		print 'X = ', X[n], '\n'
+		print 'weights_1 = ',weights_1, '\nb1 = ',b1, '\nweights_2 = ', weights_2, '\nb2 = ', b2
+		#print weights_1.shape, X[0][:,np.newaxis].shape, b1.shape
+		a1 = relu((np.dot(weights_1, X[n][:,np.newaxis]))+b1)
+		#print a1.shape
+		#print weights_2.shape, a1.shape, b2.shape
+		a2 = threshold((np.dot(weights_2, a1))+b2)
+		print 'a2 before threshold ',(np.dot(weights_2, a1))+b2, '\n'
+		print 'a2 = ',a2
+		l = loss(a2[0], Y[n])
+		print 'loss =',l
+		#print 'loss =',l 
+		weights_1, b1, weights_2, b2 = backprop(X[n], Y[n], weights_1, b1, a1, weights_2, b2, a2[0], l)
+		print '\n'
+
+predict()
+print weights_1, b1, weights_2, b2
