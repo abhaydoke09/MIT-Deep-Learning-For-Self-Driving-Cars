4477ba299a974e3101632da86b4e45c40a0e12bb
abhaydoke09 <abhaydoke09@gmail.com>
2017-07-31 16:13:30 -0400

    forward pass done

--- code/experimental/XOR_gate_with_2_layered_perceptron.py
+++ code/experimental/XOR_gate_with_2_layered_perceptron.py
@@ -20,19 +20,22 @@ def predict(X, Y, W):
 	for d in xrange(X.shape[0]):
 		print '{:^12}'.format(X[d]), '{:^9}'.format(activation(np.dot(X[d],W))), '{:^6}'.format(Y[d])
 
+def loss(Y, target):
+	return (Y - target)**2
+
+
 
 
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 Y = np.array([[0],[1],[1],[0]])
-biases = np.ones((4,1))
-X = np.hstack((X,biases))
-weights_1 = np.random.randn(2,3)
-weights_2 = np.random.randn(2,3)
-
-print weights_1, relu(weights_1)
-
-# weights = train(X,Y,weights)
-# predict(X,Y,weights)
 
 
+weights_1 = np.random.randn(3,2)
+b1 = np.random.randn(3)
+weights_2 = np.random.randn(1,3)
+b2 = np.random.randn(1)
 
+for n in range(X.shape[0]):
+	a1 = relu((np.dot(weights_1, X[n]))+b1)
+	a2 = relu((np.dot(weights_2, a1))+b2)
+	print loss(a2, Y[n])
